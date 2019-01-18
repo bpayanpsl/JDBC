dfa275a46828304773ed6b529ed408770565a41a
bpayanpsl <bpayanpsl@gmail.com>
2019-01-18 16:22:30 +0100

    Bjr

--- src/main/java/fr/dta/jdbc/App.java
+++ src/main/java/fr/dta/jdbc/App.java
@@ -1,13 +1,9 @@
 package fr.dta.jdbc;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
